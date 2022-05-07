5ed8222a06c7f5cf375cfbd5215ee56b5b325761
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-05-08 01:34:33 +0530

    in ashish branch

--- frontend/src/components/Flight.jsx
+++ frontend/src/components/Flight.jsx
@@ -1,70 +1,73 @@
-import React,{Component, useState} from "react";
+import React, { Component, useState } from "react";
+import { ImLocation } from "react-icons/im";
+import "./Flight.css";
+import { Model } from "./Model";
+import { Dropdown } from "./dropdown";
+import { IoIosArrowDown } from "react-icons/io";
+import { MdOutlineSwapHoriz } from "react-icons/md";
+import { Link } from "react-router-dom";
 
-import {ImLocation} from "react-icons/im"
-import "./Flight.css"
-import {Model} from "./Model"
-import {Dropdown} from "./dropdown"
-import{IoIosArrowDown} from "react-icons/io"
-import{MdOutlineSwapHoriz} from "react-icons/md"
-import {Link} from "react-router-dom"
+export const Flight = () => {
+  const [openmodel, setOpenmodel] = useState(false);
+  const [selected, setSelected] = useState("Economy");
 
-
-
-
-export const Flight=()=>{
-
-const [openmodel,setOpenmodel]=useState(false);
-const [selected,setSelected]=useState("Economy");
-
-const openpopup=()=>{
+  const openpopup = () => {
     setOpenmodel(true);
-}
+  };
 
-    return (
+  return (
     <div className="flightnav">
-<div className="maindiv">
-<div className="firstdiv">
-<div>Return</div>
-<div>One-way</div>
-<div>Multi-city</div> 
-</div>
-<div className="seconddiv">
-    <div></div>
-<div onClick={openpopup}>traveller <IoIosArrowDown/></div>
-<div><Dropdown  selected={selected} setSelected={setSelected}/></div>
-</div>
-</div>
-{openmodel && <Model  setOpenmodel={setOpenmodel}/>}  
+      <div className="maindiv">
+        <div className="firstdiv">
+          <div>Return</div>
+          <div>One-way</div>
+          <div>Multi-city</div>
+        </div>
+        <div className="seconddiv">
+          <div></div>
+          <div onClick={openpopup}>
+            traveller <IoIosArrowDown />
+          </div>
+          <div>
+            <Dropdown selected={selected} setSelected={setSelected} />
+          </div>
+        </div>
+      </div>
+      {openmodel && <Model setOpenmodel={setOpenmodel} />}
 
+      <div className="inputdiv">
+        <div>
+          <label className="migrate-container">
+            <ImLocation />
+            <input className="migrate" type="text" placeholder="Leaving from" />
+          </label>
+        </div>
 
+        <button className="swapbtn">
+          <MdOutlineSwapHoriz />
+        </button>
+        <div>
+          <label className="migrate-container">
+            <ImLocation />
+            <input className="migrate" type="text" placeholder="Going to" />
+          </label>
+        </div>
 
+        <div className="input-date-time">
+          <span>Departing</span>
+          <input className="date" type="date" />
+        </div>
+        <div className="input-date-time">
+          <span>Returning</span>
+          <input className="date" type="date" />
+        </div>
+      </div>
 
-<div className="inputdiv">
-   <div className="migrate-container">
-       <label><ImLocation/>
-     <input  className="migrate"  type="text"  placeholder="Leaving from"/>
-       </label>
-   </div>
-
-<button className="swapbtn"><MdOutlineSwapHoriz/></button>
-   <div className="migrate-container">
-       <label><ImLocation/>
-        <input  className="migrate"  type="text"  placeholder="Going to"/>
-       </label>
-   </div>
-    
-    <input className="date"      type="date" placeholder="Departing" />
-    <input className="date"  type="date" placeholder="Returning" />
-    
-</div>
-
-<Link to="/flightresult"><button className="navflightbtn">Search</button></Link>
-
+      <Link to="/flightresult">
+        <button className="navflightbtn">Search</button>
+      </Link>
     </div>
-    
-)
+  );
+};
 
-
-}
-
-// {openmodel && <Model/>}   it's javascript syntax when open model is true then show popup else not 
+// {openmodel && <Model/>}   it's javascript syntax when open model is true then show popup else not
--- frontend/src/components/Model.jsx
+++ frontend/src/components/Model.jsx
@@ -1,95 +1,136 @@
-import React,{Component, useState} from "react";
-import "./Model.css"
-import{BsBoxArrowInUpRight} from "react-icons/bs"
+import React, { Component, useState } from "react";
+import "./Model.css";
+import { BsBoxArrowInUpRight } from "react-icons/bs";
 
-export const Model=({setOpenmodel})=>{
+export const Model = ({ setOpenmodel }) => {
+  const [adult, setAdult] = useState(1);
 
-    const [adult,setAdult]=useState(1);
+  const [child, setChild] = useState(0);
 
-    const [child,setChild]=useState(0);
+  const [infant, setInfant] = useState(0);
 
-    const [infant,setInfant]=useState(0);
+  const closepopup = () => {
+    setOpenmodel(false);
+  };
 
-    const closepopup=()=>{
-        setOpenmodel(false);
+  const adultcountchange = (value) => {
+    if (adult === 1 && value === -1) {
+      return;
     }
+    setAdult(adult + value);
+  };
 
-    const adultcountchange=(value)=>{
-        if(adult===1 && value===-1){
-            return;
-        }
-            setAdult(adult+value);
-        
-        
+  const childcountchange = (value) => {
+    if (child === 0 && value === -1) {
+      return;
     }
+    setChild(child + value);
+  };
 
-     const childcountchange=(value)=>{
-         if(child===0 && value===-1){
-             return;
-         }
-      setChild(child+value);
+  const infantcountchange = (value) => {
+    if (infant === 0 && value === -1) {
+      return;
     }
+    setInfant(infant + value);
+  };
 
-    const infantcountchange=(value)=>{
-        if(infant===0 && value===-1){
-            return;
-        }
-setInfant(infant+value);
-    }
-
-  
-    return (
-        <div className="model">
-           <div className="popupbox1">
-          <h3 className="travellerh3tag">Travellers</h3>
-          <div className="firstcontent">
-              <div>Adults</div>
-              <div className="incdec-div">
-                  <div> <button className="decbutton" onClick={()=>{
-                      adultcountchange(-1)
-                  }}>-</button></div>
-                  <div>{adult}</div>
-                  <div><button className="incdec-button" onClick={()=>{
-                      adultcountchange(1)
-                  }}>+</button></div>
-              </div>
-             
-               </div>
-               <div className="firstcontent">
-              <div>Children</div>
-              
-              <div className="incdec-div">
-                  <div> <button className="decbutton" onClick={()=>{
-                      childcountchange(-1)
-                  }}>-</button></div>
-                  <div>{child}</div>
-                  <div><button className="incdec-button" onClick={()=>{
-                      childcountchange(1)
-                  }}>+</button></div>
-              </div>
-              </div>
-              <p className="ageptag">Ages to 17</p>
-               <div className="firstcontent">
-              <div>Infants</div>
-              <div className="incdec-div">
-                  <div> <button className="decbutton" onClick={()=>{
-                      infantcountchange(-1)
-                  }}>-</button></div>
-                  <div>{infant}</div>
-                  <div><button className="incdec-button" onClick={()=>{
-                      infantcountchange(1)
-                  }}>+</button></div>
-              </div>
-             
-               </div>
-
-               <p className="ageptag">Younger than 2</p>
-               <p className="airline-rule">Airline Age Rules <BsBoxArrowInUpRight/></p>
-
-               <button className="donebtn" onClick={closepopup}>Done</button>
-
+  return (
+    <div className="model">
+      <div className="popupbox1">
+        <h3 className="travellerh3tag">Travellers</h3>
+        <div className="firstcontent">
+          <div>Adults</div>
+          <div className="incdec-div">
+            <div>
+              {" "}
+              <button
+                className="decbutton"
+                onClick={() => {
+                  adultcountchange(-1);
+                }}
+              >
+                -
+              </button>
             </div>
+            <div>{adult}</div>
+            <div>
+              <button
+                className="incdec-button"
+                onClick={() => {
+                  adultcountchange(1);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
         </div>
-        
-    )
-}
+        <div className="firstcontent">
+          <div>Children</div>
+
+          <div className="incdec-div">
+            <div>
+              {" "}
+              <button
+                className="decbutton"
+                onClick={() => {
+                  childcountchange(-1);
+                }}
+              >
+                -
+              </button>
+            </div>
+            <div>{child}</div>
+            <div>
+              <button
+                className="incdec-button"
+                onClick={() => {
+                  childcountchange(1);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        <p className="ageptag">Ages to 17</p>
+        <div className="firstcontent">
+          <div>Infants</div>
+          <div className="incdec-div">
+            <div>
+              {" "}
+              <button
+                className="decbutton"
+                onClick={() => {
+                  infantcountchange(-1);
+                }}
+              >
+                -
+              </button>
+            </div>
+            <div>{infant}</div>
+            <div>
+              <button
+                className="incdec-button"
+                onClick={() => {
+                  infantcountchange(1);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <p className="ageptag">Younger than 2</p>
+        <p className="airline-rule">
+          Airline Age Rules <BsBoxArrowInUpRight />
+        </p>
+
+        <button className="donebtn" onClick={closepopup}>
+          Done
+        </button>
+      </div>
+    </div>
+  );
+};
