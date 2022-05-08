8b739d5ee6907e43ed7526559ea02c02c15b46ee
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-05-08 18:14:50 +0530

    Done

---
+++ frontend/src/otp.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+export const otp=(c)=>{
+    function go(){
+        // alert"Pay"
+        setInterval(() => {
+            <Link rel="stylesheet" to="/" />
+
+          }, 2000);
+        
+    }
+     return(
+        < div>
+        <input type="number" />
+        <input onClick={go} type="submit" />
+        </div>
+     )
+   
+}
